5dc61e3662b23a0d565b06965cd9989fc452dd3f
Prabhat <prabhatmani933@gmail.com>
2022-03-07 11:22:53 +0530

    Created product schema & Seeded our database.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require('path'); //to set views directory
 const mongoose = require('mongoose');
+const Product = require('./models/product'); //required our product schema
 
 mongoose.connect('mongodb://0.0.0.0:27017/farmStand')
     .then(() => {
@@ -12,13 +13,13 @@ mongoose.connect('mongodb://0.0.0.0:27017/farmStand')
         console.log(err)
     })
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //for absolute path
+app.set('view engine', 'ejs'); //told express to use EJS
 
 app.get('/dog', (req, res) => {
     res.send('WOOF!');
 })
 
-app.listen(3000, () => {
-    console.log('App Is Listening On Port 3000!');
+app.listen(5000, () => {
+    console.log('App Is Listening On Port 5000!');
 })
---
+++ seeds.js
@@ -0,0 +1,49 @@
+//This file would give data(initial) to our database(seed our database)
+
+const Product = require('./models/product'); //required our product schema
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://0.0.0.0:27017/farmStand')
+    .then(() => {
+        console.log("MONNGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO ERROR!!!!")
+        console.log(err)
+    })
+
+    const seedProducts = [
+        {
+            name: 'Fairy Eggplant',
+            price: 1.00,
+            category: 'vegetable'
+        },
+        {
+            name: 'Organic Goddess Melon',
+            price: 4.99,
+            category: 'fruit'
+        },
+        {
+            name: 'Organic Mini Seedless Watermelon',
+            price: 3.99,
+            category: 'fruit'
+        },
+        {
+            name: 'Organic Celery',
+            price: 1.50,
+            category: 'fruit'
+        },
+        {
+            name: 'Chocolate Whole Milk',
+            price: 4.99,
+            category: 'fruit'
+        }
+    ]
+    
+    Product.insertMany(seedProducts)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(e => {
+            console.log(e);
+        })
